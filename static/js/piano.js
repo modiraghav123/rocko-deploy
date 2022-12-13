074bb14bfce91fd65fb77be08aeeff3f856d1bac
unknown <ankit11hab@gmail.com>
2022-12-14 04:42:17 +0530

    Keys

--- static/js/piano.js
+++ static/js/piano.js
@@ -2,196 +2,196 @@ const audio = document.getElementById('audio');
 
 const elm1 = document.getElementById(`1_wk`);
 elm1.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/1 white key.wav`;
+    audio.src = `static/audio/White_keys/1 white key.mp3`;
     audio.play();
     console.log(`playing1`);
 });
 
 const elm2 = document.getElementById(`2_wk`)
 elm2.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/2 white key.wav`;
+    audio.src = `static/audio/White_keys/2 white key.mp3`;
     audio.play();
     console.log(`playing2`);
 });
 
 const elm3 = document.getElementById(`4_wk`)
 elm3.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/3 white key.wav`;
+    audio.src = `static/audio/White_keys/3 white key.mp3`;
     audio.play();
     console.log(`playing3`);
 });
 
 const elm4 = document.getElementById(`4_wk`)
 elm4.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/4 white key.wav`;
+    audio.src = `static/audio/White_keys/4 white key.mp3`;
     audio.play();
     console.log(`playing4`);
 });
 
 const elm5 = document.getElementById(`5_wk`)
 elm5.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/5 white key.wav`;
+    audio.src = `static/audio/White_keys/5 white key.mp3`;
     audio.play();
     console.log(`playing5`);
 });
 
 const elm6 = document.getElementById(`6_wk`)
 elm6.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/6 white key.wav`;
+    audio.src = `static/audio/White_keys/6 white key.mp3`;
     audio.play();
     console.log(`playing6`);
 });
 
 const elm7 = document.getElementById(`7_wk`)
 elm7.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/7 white key.wav`;
+    audio.src = `static/audio/White_keys/7 white key.mp3`;
     audio.play();
     console.log(`playing7`);
 });
 
 const elm8 = document.getElementById(`8_wk`)
 elm8.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/8 white key.wav`;
+    audio.src = `static/audio/White_keys/8 white key.mp3`;
     audio.play();
     console.log(`playing8`);
 });
 
 const elm9 = document.getElementById(`9_wk`)
 elm9.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/9 white key.wav`;
+    audio.src = `static/audio/White_keys/9 white key.mp3`;
     audio.play();
     console.log(`playing9`);
 });
 
 const elm10 = document.getElementById(`10_wk`)
 elm10.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/10 white key.wav`;
+    audio.src = `static/audio/White_keys/10 white key.mp3`;
     audio.play();
     console.log(`playing10`);
 });
 
 const elm11 = document.getElementById('11_wk')
 elm11.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/11 white key.wav`;
+    audio.src = `static/audio/White_keys/11 white key.mp3`;
     audio.play();
     console.log(`playing11`);
 });
 
 const elm12 = document.getElementById(`12_wk`)
 elm12.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/12 white key.wav`;
+    audio.src = `static/audio/White_keys/12 white key.mp3`;
     audio.play();
     console.log(`playing12`);
 });
 
 const elm13 = document.getElementById(`13_wk`)
 elm13.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/13 white key.wav`;
+    audio.src = `static/audio/White_keys/13 white key.mp3`;
     audio.play();
     console.log(`playing13`);
 });
 
 const elm14 = document.getElementById(`14_wk`)
 elm14.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/14 white key.wav`;
+    audio.src = `static/audio/White_keys/14 white key.mp3`;
     audio.play();
     console.log(`playing14`);
 });
 
 const elm15 = document.getElementById(`15_wk`)
 elm15.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/15 white key.wav`;
+    audio.src = `static/audio/White_keys/15 white key.mp3`;
     audio.play();
     console.log(`playing15`);
 });
 
 const elm16 = document.getElementById(`16_wk`)
 elm16.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/16 white key.wav`;
+    audio.src = `static/audio/White_keys/16 white key.mp3`;
     audio.play();
     console.log(`playing16`);
 });
 
 const elm17 = document.getElementById(`17_wk`)
 elm17.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/17 white key.wav`;
+    audio.src = `static/audio/White_keys/17 white key.mp3`;
     audio.play();
     console.log(`playing17`);
 });
 
 const elm18 = document.getElementById(`18_wk`)
 elm18.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/18 white key.wav`;
+    audio.src = `static/audio/White_keys/18 white key.mp3`;
     audio.play();
     console.log(`playing18`);
 });
 
 const elm19 = document.getElementById(`19_wk`)
 elm19.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/19 white key.wav`;
+    audio.src = `static/audio/White_keys/19 white key.mp3`;
     audio.play();
     console.log(`playing19`);
 });
 
 const elm20 = document.getElementById(`20_wk`)
 elm20.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/20 white key.wav`;
+    audio.src = `static/audio/White_keys/20 white key.mp3`;
     audio.play();
     console.log(`playing20`);
 });
 
 const elm21 = document.getElementById(`21_wk`)
 elm21.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/21 white key.wav`;
+    audio.src = `static/audio/White_keys/21 white key.mp3`;
     audio.play();
     console.log(`playing21`);
 });
 
 const elm22 = document.getElementById(`22_wk`)
 elm22.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/22 white key.wav`;
+    audio.src = `static/audio/White_keys/22 white key.mp3`;
     audio.play();
     console.log(`playing22`);
 });
 
 const elm23 = document.getElementById(`23_wk`)
 elm23.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/23 white key.wav`;
+    audio.src = `static/audio/White_keys/23 white key.mp3`;
     audio.play();
     console.log(`playing23`);
 });
 
 const elm24 = document.getElementById(`24_wk`)
 elm24.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/24 white key.wav`;
+    audio.src = `static/audio/White_keys/24 white key.mp3`;
     audio.play();
     console.log(`playing24`);
 });
 
 const elm25 = document.getElementById(`25_wk`)
 elm25.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/25 white key.wav`;
+    audio.src = `static/audio/White_keys/25 white key.mp3`;
     audio.play();
     console.log(`playing25`);
 });
 
 const elm26 = document.getElementById(`26_wk`)
 elm26.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/26 white key.wav`;
+    audio.src = `static/audio/White_keys/26 white key.mp3`;
     audio.play();
     console.log(`playing26`);
 });
 
 const elm27 = document.getElementById(`27_wk`)
 elm27.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/27 white key.wav`;
+    audio.src = `static/audio/White_keys/27 white key.mp3`;
     audio.play();
     console.log(`playing27`);
 });
 
 const elm28 = document.getElementById(`28_wk`)
 elm28.addEventListener("mouseenter",function(){
-    audio.src = `static/audio/White_keys/28 white key.wav`;
+    audio.src = `static/audio/White_keys/28 white key.mp3`;
     audio.play();
     console.log(`playing18`);
 });
